8af92b87144b7359563723c29c148dee9fd2c892
Adrian Visarra <adrianv@spotify.com>
2020-08-29 16:11:30 +0200

    Add todo form in the todo list component

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
+import NewTodoForm from './NewTodoForm';
 
 import './TodoList.css';
 
 const TodoList = ({ todos }) => (
   <div className="list-wrapper">
+    <NewTodoForm />
     {todos.map(todo => <TodoListItem todo={todo} />)}
   </div>
 );
