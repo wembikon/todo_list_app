4a07419f308e8d8f136bb8f9a187fee5fd55ec3a
Adrian Visarra <adrianv@spotify.com>
2020-08-29 16:13:52 +0200

    Add todo list component to the root app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
+
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>Hello, React!</h1>
+    <TodoList />
   </div>
 );
 
